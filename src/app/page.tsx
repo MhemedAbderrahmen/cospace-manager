d55ddb8da33e536d4c8696347512f15bc645574a
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-07-15 21:52:18 +0100

    clerk integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { HydrateClient } from "~/trpc/server";
 
 export default function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div>Cospace</div>
+        <div className="flex flex-row items-center justify-center space-x-4">
+          <div>Cospace</div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </main>
     </HydrateClient>
   );
