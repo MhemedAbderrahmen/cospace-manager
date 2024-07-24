bc363881ec4b0ec79ea8a43ddd3efbe67a926a93
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2024-07-24 17:44:01 +0200

    new theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,22 @@ import { HydrateClient } from "~/trpc/server";
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="p-4">
-        <Link href={"/dashboard"}>
-          <Button>Dashboard</Button>
-        </Link>
+      <main className="flex h-full w-full flex-col items-center p-4">
+        <section className="flex h-screen max-w-screen-md flex-col items-center justify-center gap-4">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            The best way to find{" "}
+            <span className="text-primary underline">coworking spaces</span> and
+            manage them
+          </h1>
+          <p className="mb-8 text-xl">
+            Easily book coworking spaces and meeting rooms with our apqp.
+          </p>
+          <Link href={"/dashboard"}>
+            <Button variant={"default"} size={"lg"}>
+              Get Started
+            </Button>
+          </Link>
+        </section>
       </main>
     </HydrateClient>
   );
